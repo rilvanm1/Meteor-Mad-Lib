b60b52177c008d923330aaa1dd749f0df4e81a6a
DellAgli <anthony.dellagli@gmail.com>
2016-04-04 20:00:29 -0400

    Log the madlib

--- client/madLib.js
+++ client/madLib.js
@@ -5,6 +5,17 @@ Template.body.events({
 
 		event.preventDefault();
 
-		console.log("Hello World");
+		//console.log("Hello World");
+
+		const target = event.target;
+
+		const exclamation = target.exclamation.value;
+		const adverb = target.adverb.value
+		const animal = target.animal.value
+		const verb = target.verb.value
+		
+
+
+		console.log(exclamation + "! you say, as you " + adverb + " jump onto your " + animal + " and " + verb + " into the sunset");
 	}
 })
