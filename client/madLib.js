ebe9361c7fe65ed322cba05419d526aab503585f
Kalyan R Parajuli <kalyanranjanp@gmail.com>
2016-04-04 20:30:36 -0400

    Completed

--- client/madLib.js
+++ client/madLib.js
@@ -2,11 +2,9 @@ import './madLib.html';
 
 Template.body.events({
 	'submit form'(event){
-
 		event.preventDefault();
 
 		//console.log("Hello World");
-
 		const target = event.target;
 
 		const exclamation = target.exclamation.value;
@@ -14,8 +12,13 @@ Template.body.events({
 		const animal = target.animal.value
 		const verb = target.verb.value
 		
+		const t = `'${exclamation}'! you say, as you ${adverb} jump onto your ${animal} and ${verb} into the sunset`;
 
+		$('h1').text(t);
+		$('h1').css('color','#0000ff');
 
-		console.log(exclamation + "! you say, as you " + adverb + " jump onto your " + animal + " and " + verb + " into the sunset");
+		$('h1').hide();
+		$('#headText').text(t).fadeIn(1000);
+
 	}
 })
